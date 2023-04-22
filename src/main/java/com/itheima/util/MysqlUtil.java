6d1da0238502902bc9a968ad0b52bc0a4a39a43f
西域情人 <jitongyu0607@126.com>
2023-04-23 01:14:02 +0800

    初次提交springboot和mybatis的文件，內容為增刪改查

--- src/main/java/com/itheima/util/MysqlUtil.java
+++ src/main/java/com/itheima/util/MysqlUtil.java
@@ -12,6 +12,10 @@ public class MysqlUtil {
      * 寫一個工具欄，直接設置當前日期
      */
     public static void setUpdateDate(Emp emp){
+
+        /**
+         *
+         */
         Date date=new Date();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         String detetime = simpleDateFormat.format(date);
